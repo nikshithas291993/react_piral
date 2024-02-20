1ce8d4f38734ab9ca13d3ca81f42eee969a39d08
nikshithas291993 <nikshitha.s@sonata-software.com>
2024-02-20 21:58:21 +0530

    added port change

--- homepage/src/component/config/config.js
+++ homepage/src/component/config/config.js
@@ -1,5 +1,5 @@
 const sharedConfig = {
-    REACT_APP_API_URL: 'http://localhost:8080',
+    REACT_APP_API_URL: 'http://localhost:8000',
     REACT_APP_USER_API_URL: 'http://localhost:5000',
   };
   
