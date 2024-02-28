95455e1a2555046170511d5a290f410b9d4828d3
nikshithas291993 <nikshitha.s@sonata-software.com>
2024-02-28 15:28:44 +0530

    update on config

---
+++ app-shell/src/config/config.js
@@ -0,0 +1,9 @@
+const sharedConfig = {
+    REACT_APP_API_URL: 'http://localhost:3002',
+    CHATBOT_URL: 'http://localhost:3002',
+    MICROSERVICES: ['GrandSale', 'microservice2'],
+    ISDEMO: true,
+  };
+  
+export default sharedConfig;
+  
